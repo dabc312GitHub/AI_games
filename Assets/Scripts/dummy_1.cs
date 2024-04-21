3c979bde292f08d9762588e6f84cb30343f072f1
dabc312GitHub <daryl.butron@ucsp.edu.pe>
2024-04-21 13:40:35 -0500

    assets ingreso

--- Assets/Scripts/dummy_1.cs
+++ Assets/Scripts/dummy_1.cs
@@ -7,7 +7,7 @@ public class dummy_1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //
     }
 
     // Update is called once per frame
